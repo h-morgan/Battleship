4158666b73f75c53a647d4884e1a50bde9ca3b65
Haley Morgan <haley.morgan@bostonfusion.com>
2020-02-25 22:29:06 -0500

    add tests"

--- src/battleship/Tests.java
+++ src/battleship/Tests.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Tests {
 
 	public static void main(String[] args) {
-		Ship x = new EmptySea();
+		Ship x = new BattleShip();
 		System.out.println("Ship type: " + x.getShipType());
 		System.out.println("Length: " + x.getLength());
 		System.out.println(Arrays.toString(x.getHit()));
