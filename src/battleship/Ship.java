b09c69b2f6a3d152eb0dbec5a359bc53e599fb20
Haley Morgan <haley.morgan@bostonfusion.com>
2020-02-29 13:21:29 -0500

    add method stubs for Ship class

--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -40,7 +40,68 @@ public abstract class Ship {
 		this.hit = hit;
 	}
 	
+	//abstract methods - to be defined in subclasses
 	abstract String getShipType();
+	
+	//regular methods - for all ships
+	/**	
+	 * Returns true if it is okay to put a ship of this length with its bow in this location, 
+	 * With the given orientation, and false otherwise
+	 * @param row
+	 * @param column
+	 * @param horizontal
+	 * @param ocean
+	 * @return
+	 */
+	public boolean okToPlaceShipAt(int row, int column, boolean horizontal, Ocean ocean) {
+		return true;
+	}
+	
+
+	/**
+	 * "Puts" ship in the ocean 
+	 * This involves going to bowRow, bowColumn, and horizontal instance variables
+	 * Also involves putting a reference to the ship in each of 1 or more locations (up to 8)
+	 * @param row - row to place ship in
+	 * @param column - column to place ship in
+	 * @param horizontal - true if orientation of ship is horizontal, else false
+	 * @param ocean
+	 */
+	public void placeShipAt(int row, int column, boolean horizontal, Ocean ocean) {
+	}
+	
+	/**
+	 * If part of the ship occupies the given row and column, and the ship hasn't been sunk
+	 * Mark that part of the ship as "hit" (in the array, 0 indicates the bow) 
+	 * And return true, otherwise false
+	 * @param row
+	 * @param column
+	 * @return
+	 */
+	public boolean shootAt(int row, int column) {
+		return true;
+	}
+	
+	
+	/**
+	 * @return true if every part of the ship has been hit, false otherwise
+	 */
+	public boolean isSunk() {
+		return true;
+	}
+	
+	/**
+	 * Returns a single character string to use in the Ocean's print method
+	 * Should return X if the ship has been sunk, S if it has not been sunk
+	 * Can be used to print out locations in the ocean that have been shot at 
+	 */
+	@Override
+	public String toString() {
+		return "X";
+	}
+	
+	
+	
 	
 	
 	
