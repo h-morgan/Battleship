51dd27d863589345a9e90d41c85cae347dbcac7a
Haley Morgan <haley.morgan@bostonfusion.com>
2020-03-01 18:22:29 -0500

    add ocean method stubs

--- src/battleship/Ocean.java
+++ src/battleship/Ocean.java
@@ -6,10 +6,51 @@ public class Ocean {
 	int hitCount;
 	int shipsSunk;
 	
+	// Constructor
 	public Ocean() {
 		this.shotsFired = 0;
 		this.hitCount = 0;
+	}
+	
+	public void placeAllShipsRandomly() {
 		
+	}
+	
+	public boolean isOccupied(int row, int column) {
+		return true;
+	}
+	
+	
+	public boolean shootAt(int row, int column) {
+		return true;
+	}
+	
+	
+	public int getShotsFire() {
+		return shotsFired;
+	}
+	
+	
+	public int getHitCount() {
+		return hitCount;
+	}
+	
+	
+	public int getsShipsSunk() {
+		return shipsSunk;
+	}
+	
+	
+	public boolean isGameOver() {
+		return false;
+	}
+	
+	
+	public Ship[][] getShipArray() {
+		return ship;
+	}
+	
+	public void print() {
 		
 	}
 
